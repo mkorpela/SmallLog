a26efda06e11fef2d2404632c7a9c0d10c2fe9ed
Mikko Korpela <mikko.korpela@gmail.com>
2014-10-05 22:49:11 +0300

    sqlite3.db

--- SmallLog.py
+++ SmallLog.py
@@ -1,34 +1,60 @@
+import os
+import sqlite3
+import sys
+
 
 class SmallLog(object):
 
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self, log_name='output.db'):
+        self.__connection = None
         self._log_name = log_name
+        self._parents = []
+
+    @property
+    def _connection(self):
+        if self.__connection is None:
+            if os.path.exists(self._log_name):
+                os.remove(self._log_name)
+            self.__connection = sqlite3.connect(self._log_name)
+            self._create_db_tables(self.__connection)
+        return self.__connection
+
+    def _create_db_tables(self, connection):
+        connection.execute('CREATE TABLE suites(name)')
+        connection.execute('CREATE TABLE tests(name)')
+        connection.execute('CREATE TABLE keywords(name, parent)')
+        connection.commit()
 
     def start_suite(self, name, attributes):
-        print name, attributes
+        self._connection.execute('INSERT INTO suites (name) VALUES (?)', [name])
+        self._connection.commit()
 
     def end_suite(self, name, attributes):
         pass
 
     def start_test(self, name, attributes):
-        pass
+        self._connection.execute('INSERT INTO tests (name) VALUES (?)', [name])
+        self._connection.commit()
+        self._parents.append(name)
 
     def end_test(self, name, attributes):
-        pass
+        self._parents.pop()
 
     def start_keyword(self, name, attributes):
-        pass
+        self._connection.execute('INSERT INTO keywords (name, parent) VALUES (?, ?)', [name, self._parents[-1]])
+        self._connection.commit()
+        self._parents.append(name)
 
     def end_keyword(self, name, attributes):
-        pass
+        self._parents.pop()
 
     def log_message(self, message):
         pass
 
     def close(self):
-        pass
+        self._connection.close()
 
 
 class SmallLogResult(object):
@@ -39,7 +65,14 @@ class SmallLogResult(object):
     @property
     def suite(self):
         suite = lambda:0
-        suite.name = 'Test'
+        with sqlite3.connect(self._log_name) as c:
+            suite.name = c.execute('SELECT name FROM suites').fetchone()[0]
+            suite.tests = []
+            for test in c.execute('SELECT name FROM tests').fetchall():
+                t = lambda:0
+                t.name = test[0]
+                t.keywords = c.execute('SELECT name FROM keywords WHERE parent=?', [t.name]).fetchall()
+                suite.tests.append(t)
         return suite
 
 
@@ -48,3 +81,5 @@ if __name__ == '__main__':
     expected = ExecutionResult('output.xml')
     actual = SmallLogResult('output.db')
     assert expected.suite.name == actual.suite.name
+    assert len(expected.suite.tests) == len(actual.suite.tests)
+    assert len(expected.suite.tests[0].keywords) == len(actual.suite.tests[0].keywords)
